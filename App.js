34d580af21fbe57a5bbc9baa78b04a54ddd65204
sunwasan <105874427+sunwasan@users.noreply.github.com>
2024-02-14 23:33:40 +0700

    change folder week02 => week01

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './screens/Welcome';
-import Exercise1 from './screens/week02/Exercise1';
-import Exercise2 from './screens/week02/Exercise2';
-import Exercise3 from './screens/week02/Exercise3';
-import Exercise4 from './screens/week02/Exercise4';
-import Exercise5 from './screens/week02/Exercise5';
-import Exercise6 from './screens/week02/Exercise6';
-import Exercise7 from './screens/week02/Exercise7';
-import Exercise8 from './screens/week02/Exercise8';
-import Exercise9 from './screens/week02/Exercise9';
-import Exercise10 from './screens/week02/Exercise10';
-import Exercise11 from './screens/week02/Exercise11';
-import Exercise12 from './screens/week02/Exercise12';
-import Extra from './screens/week02/Extra';
+import Exercise1 from './screens/week01/Exercise1';
+import Exercise2 from './screens/week01/Exercise2';
+import Exercise3 from './screens/week01/Exercise3';
+import Exercise4 from './screens/week01/Exercise4';
+import Exercise5 from './screens/week01/Exercise5';
+import Exercise6 from './screens/week01/Exercise6';
+import Exercise7 from './screens/week01/Exercise7';
+import Exercise8 from './screens/week01/Exercise8';
+import Exercise9 from './screens/week01/Exercise9';
+import Exercise10 from './screens/week01/Exercise10';
+import Exercise11 from './screens/week01/Exercise11';
+import Exercise12 from './screens/week01/Exercise12';
+import Extra from './screens/week01/Extra';
 
 export default function App(){
   return(
